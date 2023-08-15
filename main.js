b78015867dc39dae320425b51fb435ef2338521c
Tanvi Bhabal <81086155+tanvi1911@users.noreply.github.com>
2023-08-15 16:43:18 +0530

    third_commit

--- main.js
+++ main.js
@@ -15,3 +15,41 @@ const closeNav = () => {
 }
 
 closeNavBtn.addEventListener('click', closeNav);
+
+if(window.innerWidth < 1024) {
+    document.querySelectorAll('#nav__items li a').forEach(navItem => {
+        navItem.addEventListener('click', () => {
+            closeNav();
+        })
+    })
+}
+
+// change navbar styles on scroll
+window.addEventListener('scroll', () => {
+    document.querySelector('nav').classList.toggle('window-scroll', window.scrollY > 0)
+})
+
+
+// TESTIMONIAL SECTION
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+    //RESPONSIVE BREAKPOINTS
+    breakpoints: {
+        // when window width is >= 600px
+        600: {
+            slidesPerView: 2
+        },
+
+        //window width is >= 600px
+        1024: {
+            slidesPerView: 3
+        }
+    }
+
+  });
